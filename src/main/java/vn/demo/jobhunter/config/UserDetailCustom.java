e9de268ee96a3be36822b5f858ab58709dfe64c0
hoangthai2002 <thaidepgiai2002@gmail.com>
2025-07-01 15:39:08 +0700

    update service

--- src/main/java/vn/demo/jobhunter/config/UserDetailCustom.java
+++ src/main/java/vn/demo/jobhunter/config/UserDetailCustom.java
@@ -1,4 +1,4 @@
-package vn.demo.jobhunter.service;
+package vn.demo.jobhunter.config;
 
 import java.util.Collections;
 
@@ -7,6 +7,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import vn.demo.jobhunter.service.UserService;
 
 @Component("userDetailsService")
 public class UserDetailCustom implements UserDetailsService {
